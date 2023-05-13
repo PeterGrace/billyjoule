c4941de7da5ab6b31343020327864c0a25fa8c10
Peter Grace <pete.grace@gmail.com>
2023-05-13 02:25:49 +0000

    emoji adding

--- src/controllers/mod.rs
+++ src/controllers/mod.rs
@@ -1,7 +1,36 @@
 use anyhow::{bail, Result};
+use s3::creds::Credentials;
+use s3::error::S3Error;
+use s3::{Bucket, Region};
+use serenity::model::guild::Guild;
 use tracing::debug;
 
 pub(crate) async fn upload_emoji(message: &String) -> Result<()> {
     debug!("{message}");
-    bail!("Not implemented yet.");
+    let args: Vec<&str> = message.split(" ").collect();
+    debug!("emoji: {}", args[1]);
+    let bucket = Bucket::new(
+        "emoji",
+        Region::Custom {
+            region: "us-east-1".to_owned(),
+            endpoint: "https://s3.vsix.me:9000".to_owned(),
+        },
+        Credentials::default()?,
+    )?
+    .with_path_style();
+    let file_list = match bucket
+        .list(format! {"{}/", args[1]}, Some("/".to_owned()))
+        .await
+    {
+        Ok(s) => s,
+        Err(e) => {
+            bail!("{}", e);
+        }
+    };
+    let link = format!(
+        "https://s3.vsix.me:9000/emoji/{}",
+        file_list[0].contents[0].key
+    );
+    debug!("{link}");
+    bail!("not implemented yet");
 }
